bb90b446b27035d78584ea7f6487296074bc498f
Rina372 <114817013+Rina372@users.noreply.github.com>
2023-04-05 16:53:27 +0300

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,39 @@
+import types
+
+
+def flat_generator(list_of_lists):
+    for sub_list in my_list:
+        for elem in sub_list:
+            yield elem
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+
+    my_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None],
+    ]
+
+    for item in flat_generator(my_list):
+        print(item)
